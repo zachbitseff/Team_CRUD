08234253e754a68f79f70785068177a06f8c6af6
Zachary Bitseff <zachbitseff@gmail.com>
2019-02-19 00:56:02 -0800

    Created ProjectsDB

---
+++ ProjectsDB/Projects.sql
@@ -0,0 +1,22 @@
+﻿use master
+
+IF DB_ID('ProjectsDB') IS NOT NULL
+	DROP DATABASE ProjectsDB
+GO
+
+CREATE DATABASE ProjectsDB
+GO
+
+USE ProjectsDB
+GO
+
+CREATE TABLE Projects
+(
+	ProjectID	int	PRIMARY KEY IDENTITY
+	,Name		varchar(50)	NOT NULL
+	,Description	varchar(300)	NOT NULL
+)
+GO
+
+INSERT INTO Projects(Name, Description)
+	VALUES('SuperSecret', 'Ultimate secretive project for automated packing')
