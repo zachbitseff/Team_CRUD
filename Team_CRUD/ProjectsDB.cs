fc83ce6067ac280022198586dfb4f5e17e98a5be
Zachary Bitseff <zachbitseff@gmail.com>
2019-02-19 05:09:49 -0800

    App.conf edit and static class

---
+++ Team_CRUD/ProjectsDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_CRUD
+{
+    static class ProjectsDB
+    {
+
+        public static List<Projects> GetProjects()
+        {
+            ProjectContext context = new ProjectContext();
+
+            List<Projects> allProjects = context.Projects.ToList();
+
+        }
+
+        public static void AddProject(Projects p)
+        {
+            ProjectContext context = new ProjectContext();
+
+            context.Projects.Add(p);
+            context.SaveChanges();
+        }
+    }
+}
